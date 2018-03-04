af53a5ac169a0281214c8b393498f84c635c938c
Lukas <lukas@youtuberis.lt>
2018-03-04 09:51:00 +0100

    updated Buefy imports

--- src/js/buefy.js
+++ src/js/buefy.js
@@ -1,22 +1,49 @@
-import Vue from 'vue';
 /*
  * Buefy Components
+ * https://buefy.github.io/#/documentation/customization
  * import Buefy from 'buefy';
  */
+ 
+
+/* 
+ * ----------
+ *  DEFAULTS
+ * ----------
+ */
+
 import config, { setOptions } from 'buefy/src/utils/config'
 setOptions(Object.assign(config, {
-	defaultContainerElement: null,
-    defaultIconPack: 'mdi',
-    defaultSnackbarDuration: 3500,
-    defaultToastDuration: 2000,
-    defaultTooltipType: 'is-primary',
-    defaultTooltipAnimated: false,
-    defaultInputAutocomplete: 'on',
-    defaultDateFormatter: null,
-    defaultDateParser: null
+    // defaultContainerElement: null,
+    // defaultIconPack: 'mdi',
+    // defaultDialogConfirmText: null,
+    // defaultDialogCancelText: null,
+    // defaultSnackbarDuration: 3500,
+    // defaultToastDuration: 2000,
+    // defaultTooltipType: 'is-primary',
+    // defaultTooltipAnimated: false,
+    // defaultInputAutocomplete: 'on',
+    // defaultDateFormatter: null,
+    // defaultDateParser: null,
+    // defaultDayNames: null,
+    // defaultMonthNames: null,
+    // defaultFirstDayOfWeek: null,
+    // defaultUnselectableDaysOfWeek: null,
+    // defaultTimeFormatter: null,
+    // defaultTimeParser: null,
+    // defaultModalScroll: null,
+    // defaultDatepickerMobileNative: true,
+    // defaultTimepickerMobileNative: true,
+    defaultNoticeQueue: false	
 }));
 
-// import { Checkbox } from 'buefy/src/components/checkbox'
+
+/* 
+ * ------------
+ *  COMPONENTS
+ * ------------
+ */
+
+// import { Checkbox, CheckboxButton } from 'buefy/src/omponents/checkbox'
 // Vue.component(Checkbox.name, Checkbox)
 import { Dropdown, DropdownItem } from 'buefy/src/components/dropdown';
 Vue.component(Dropdown.name, Dropdown);
@@ -45,6 +72,8 @@ import Select from 'buefy/src/components/select'
 Vue.component(Select.name, Select);
 import Switch from 'buefy/src/components/switch'
 Vue.component(Switch.name, Switch);
+// import Taginput from 'buefy/src/components/taginput'
+// import Timepicker from 'buefy/src/components/timepicker'
 // import Tooltip from 'buefy/src/components/tooltip'
 // import Upload from 'buefy/src/components/upload'
 
@@ -54,7 +83,7 @@ Vue.component(Switch.name, Switch);
 // Vue.prototype.$loading = LoadingProgrammatic
 // import ModalProgrammatic, { Modal } from 'buefy/src/components/modal'
 // Vue.prototype.$modal = ModalProgrammatic
-// import Snackbar from 'buefy/src/components/snackbar'
-// Vue.prototype.$snackbar = Snackbar
-// import Toast from 'buefy/src/components/toast'
-// Vue.prototype.$toast = Toast;
+import Snackbar from 'buefy/src/components/snackbar'
+Vue.prototype.$snackbar = Snackbar
+import Toast from 'buefy/src/components/toast'
+Vue.prototype.$toast = Toast;
